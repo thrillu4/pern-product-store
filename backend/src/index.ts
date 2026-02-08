21b00429e1ab56c7b39bc000ddb0f052699a23fa
Kraiovyi Denys <tonopik@gmail.com>
2026-02-09 00:15:32 +0200

    add deployment setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware } from '@clerk/express'
 import cors from 'cors'
 import express from 'express'
+import path from 'path'
 import { ENV } from './config/env'
 import commentRoutes from './routes/commentRoutes'
 import productRoutes from './routes/productRoutes'
@@ -13,9 +14,14 @@ app.use(clerkMiddleware())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
+app.get('/api/healthcheck', (req, res) => {
 	res.json({
-		message: 'Hello there!',
+		message: 'Hello there and welcome to our Shop API 🥇!',
+		endpoints: {
+			users: '/api/users',
+			products: '/api/products',
+			comments: '/api/comments',
+		},
 	})
 })
 
@@ -23,6 +29,13 @@ app.use('/api/users', userRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/comments', commentRoutes)
 
+if (ENV.NODE_ENV === 'production') {
+	const __dirname = path.resolve()
+	app.use(express.static(path.join(__dirname, '../frontend/dist')))
+	app.get('/{*any}', (req, res) => {
+		res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+	})
+}
 app.listen(ENV.PORT, () => {
 	console.log(`Server is up ✅ and running 🏃 on Port: ${ENV.PORT}`)
 })
